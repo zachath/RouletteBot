59612567d0410de289964208af17b6bd4464df32
zachath <zachariasthorell@gmail.com>
2022-07-14 18:22:09 +0200

    Comments for Game.java.

--- src/main/java/bot/RouletteBot.java
+++ src/main/java/bot/RouletteBot.java
@@ -12,8 +12,7 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 /*
  * TODO:
- *       * Testa fler än 1 spelare (Ludvig).
- *       * Implementera att det hålls koll på vilka användare som varit med på vilken match (inkluderar bridge table i databasen).
+ *       * Testa fler än 1 spelare.
  *       * Hur fungerar logging och varför det är nödvändigt.
  *       * Get the games I have played in (JOIN on the bridge table).
  * */
--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -10,16 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-//TODO: Comment this file.
-
 /**
  * Represents a individual game.
  */
 public class Game {
     private final static int PLAYER_LIMIT = 3;
     private final static int CYLINDER_SIZE = 6;
-    private final List<User> activePlayers = new ArrayList<>();
-    private List<User> allPlayers;
+    private final List<User> activePlayers = new ArrayList<>(); //The active players of the game, when a player dies or leave they are removed.
+    private List<User> allPlayers; //All players, is not modified after game start.
 
     private User leader;
     private TypeOfGame type = TypeOfGame.UNDECLARED;
@@ -45,6 +43,11 @@ public class Game {
         id = textChannel.getId() + dateTime;
     }
 
+    /**
+     * Adds a player to the game. Returns false if game is already full, otherwise true.
+     * @param player User to add to game.
+     * @return if user was added.
+     */
     public boolean addPlayer(User player) {
         if (activePlayers.size() == PLAYER_LIMIT) {
             return false;
@@ -55,6 +58,10 @@ public class Game {
         }
     }
 
+    /**
+     * Removes a player from the game, game is ended if all have left.
+     * @param player User to remove.
+     */
     public void dropPlayer(User player) {
         activePlayers.remove(player);
 
@@ -66,6 +73,10 @@ public class Game {
         }
     }
 
+    /**
+     * Main game mechanic, clickUntilFire is decremented and compared to 0, if true a bullet is said to be fired.
+     * @return if "bullet is fire or not."
+     */
     public boolean pullTrigger() {
         pulls++;
         current++;
@@ -77,14 +88,25 @@ public class Game {
         return --clicksUntilFire == 0;
     }
 
+    /**
+     * @return the active players of the game.
+     */
     public List<User> getActivePlayers() {
         return activePlayers;
     }
 
+    /**
+     * @return all players that was in the game at start.
+     */
     public List<User> getAllPlayers() {
         return allPlayers;
     }
 
+    /**
+     * Checks if the specified user is in the game or not.
+     * @param user User to check.
+     * @return If the user is in the game or not.
+     */
     public boolean contains(User user) {
         for (User player : activePlayers) {
             if (player.equals(user)) {
@@ -102,6 +124,10 @@ public class Game {
         return type;
     }
 
+    /**
+     * Returns the current player, if current is out of bounds it is reset.
+     * @return the current player.
+     */
     public User getCurrentPlayer() {
         if (current >= activePlayers.size()) {
             current = 0;
@@ -110,19 +136,33 @@ public class Game {
         return activePlayers.get(current);
     }
 
+    /**
+     * Sets new leader for the game, will be the one who joined after last leader.
+     * @return the discord mention of the new leader.
+     */
     public String newLeader() {
         leader = activePlayers.get(0);
         return leader.getAsMention();
     }
 
+    /**
+     * @return status of game.
+     */
     public boolean isLive() {
         return status;
     }
 
+    /**
+     * If only 1 chamber is left to fire, then one "wins".
+     * @return if all but one cylinder has been fired.
+     */
     public boolean onlyOneChamberLeft() {
         return pulls == CYLINDER_SIZE - 1;
     }
 
+    /**
+     * Starts the game.
+     */
     public void goLive() {
         status = true;
         if (activePlayers.size() > 1) {
